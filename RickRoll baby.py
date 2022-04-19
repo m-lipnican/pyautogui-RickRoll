a531866acde410ab8f82d7a80eae8b8957c33915
L1tusK1ng <matuslipnican396@gmail.com>
2022-04-19 18:07:56 +0200

    first commit

---
+++ RickRoll baby.py
@@ -0,0 +1,9 @@
+import pyautogui
+pyautogui.PAUSE = 1
+#print(pyautogui.position())
+pyautogui.moveTo(913, 1079, 1)
+pyautogui.rightClick(898, 1052)
+pyautogui.click(881,863)
+pyautogui.click(319, 64)
+pyautogui.typewrite("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+pyautogui.press("enter")
